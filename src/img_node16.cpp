8437f16c62ff12e7ad2fb6747d4f0d0f9d2b7e93
Isaac Watts <Isaac.s.watts@gmail.com>
2023-07-20 11:30:06 -0400

    modified img_node16 for step

--- src/img_node16.cpp
+++ src/img_node16.cpp
@@ -1,12 +1,6 @@
 /**
- * @file
- * @brief Example node to visualize range, ambient and intensity images
- *
- * Publishes ~/range_image, ~/ambient_image, and ~/intensity_image.  Please bear
- * in mind that there is rounding/clamping to display 8 bit images. For computer
- * vision applications, use higher bit depth values in /os_cloud_node/points
- */
-
+ * WARNING: Don't use 16 bit images in image_view. Use an image transport -> png instead. 
+*/
 #include <pcl/conversions.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -34,16 +28,31 @@ namespace sensor = ouster::sensor;
 namespace viz = ouster::viz;
 
 using pixel_type = uint16_t;
-constexpr size_t bit_depth = 8 * sizeof(pixel_type);
+constexpr size_t bit_depth = 8 * sizeof(pixel_type); //sizeof(pixel_type) = 2 for 16 bit
 const size_t pixel_value_max = std::numeric_limits<pixel_type>::max();
+
+sensor_msgs::Image range_image;
+sensor_msgs::Image ambient_image;
+sensor_msgs::Image intensity_image;
+
+// void ptRangeUtest(int range, double range_mult){
+//         /**Calculated by:
+//         *     double pixel_value = std::min(std::round(pt.range * range_multiplier * 1.0/1000.0 * pixel_value_max), static_cast<double>(pixel_value_max));
+//         * reinterpret_cast<pixel_type*>( //use a unit test to get a pt.range
+//         *     range_image.data.data())[u * W + v] =
+//         * pixel_value_max - pixel_value;
+//         **/
+       
+// }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "img_node");
     ros::NodeHandle nh("~");
+
     double denom;
-     nh.param("range", denom, 200.0);
-    double range_multiplier = 
-    1.0 / denom;  
+    nh.param("range", denom, 200.0);
+    double range_multiplier = 1.0 / denom;  
+
     //Default values:
         // std::vector<int> offset_default = {9, 6, 3, 0, 9, 6,
         //                                 3, 0, 9, 6, 3, 0,
@@ -84,43 +93,30 @@ int main(int argc, char** argv) {
             return EXIT_FAILURE;
         }
         else{
-            // ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("metadata", 1000);
-
             ROS_WARN("Getting info from a client");
             auto info = sensor::parse_metadata(cfg.response.metadata);
-
-            // chatter_pub.publish(cfg.response.metadata);
             
             FinalH = info.format.pixels_per_column;
             FinalW = info.format.columns_per_frame;
             Final_offset_default = info.format.pixel_shift_by_row;
 
-            ROS_FATAL("GOT DATA FROM THE CONFIG service:\nHeight: %ld Width: %ld", FinalH, FinalW);
         }
     }
     else{
         if(ros::param::has("~px_offset") && ros::param::has("~pixels_per_column") && ros::param::has("~pixels_per_row")){
-            //DEBUGGING ROS params
-            ROS_WARN("Getting Height, width, and pixel offset from rosparams");
             int tempH, tempW;
             std::vector<int> offset_temp;
 
-            // ROS_WARN("Can get ROS Param? /px_offset: %d", ros::param::has("~px_offset"));
-            // ROS_WARN("Can get ROS Param? /pixels_per_column: %d", ros::param::has("~pixels_per_column"));
-            // ROS_WARN("Can get ROS Param? /pixels_per_row: %d", ros::param::has("~pixels_per_row"));
-
             nh.param<int>("pixels_per_column", tempH, 64);
             nh.param<int>("pixels_per_row", tempW, 512);
-            //nh.param<std::vector<int>>("px_offset", offset_temp);
-            ros::param::get("~px_offset", offset_temp);
-            // ros::param::get("/pixels_per_column", tempH);
-            // ros::param::get("/pixels_per_row", tempW);
 
-            ROS_FATAL("tempH: %d tempW: %d", tempH, tempW);
+
+            ros::param::get("~px_offset", offset_temp);
+
             Final_offset_default = offset_temp;
             FinalW = (size_t) tempW;
             FinalH = (size_t) tempH;
-            ROS_FATAL("GOT DATA FROM THE PARAMS:\nHeight: %ld\nWidth: %ld", FinalH, FinalW);
+
         }
         else{
             ROS_ERROR("COULDN'T GET ROSPARAMS: \npx_offset\npixels_per_column\npixels_per_row");
@@ -132,8 +128,6 @@ int main(int argc, char** argv) {
     const std::vector<int> px_offset = Final_offset_default;
     size_t W =  FinalW;
     size_t H = FinalH;
-
-    ROS_FATAL("FINAL NUMBERS FOR HEIGHT: %ld and WIDTH: %ld", H, W);
 
     ros::Publisher range_image_pub =
         nh.advertise<sensor_msgs::Image>("range_image", 100);
@@ -150,7 +144,36 @@ int main(int argc, char** argv) {
     std::stringstream encoding_ss;
     encoding_ss << "mono" << bit_depth;
     std::string encoding = encoding_ss.str();
+    //OG step = (W*H*Bit_depth)/8
+     range_image.width = W;
+        range_image.height = H;
+        range_image.step = (W * bit_depth)/8;
+        // ROS_INFO("range_image.step: %d", range_image.step);
+        // ROS_INFO("bit depth: %ld", bit_depth);
+        //range_image.step = W;
+        range_image.encoding = encoding;
+        range_image.data.resize(W * H * bit_depth /
+                                (8 * sizeof(*range_image.data.data())));
+        
+        // ROS_INFO("range_image.data.data().size(): %ld", range_image.data.size());
 
+
+        ambient_image.width = W;
+        ambient_image.height = H;
+        ambient_image.step = (W * bit_depth)/8;
+        //ambient_image.step = W;
+        ambient_image.encoding = encoding;
+        ambient_image.data.resize(W * H * bit_depth /
+                                  (8 * sizeof(*ambient_image.data.data())));
+
+
+        intensity_image.width = W;
+        intensity_image.height = H;
+        intensity_image.step = (W * bit_depth)/8;
+        //intensity_image.step = W;
+        intensity_image.encoding = encoding;
+        intensity_image.data.resize(W * H * bit_depth /
+                                    (8 * sizeof(*intensity_image.data.data())));
     auto cloud_handler = [&](const sensor_msgs::PointCloud2::ConstPtr& m) {
         // only publish if somebody is listening
         if (range_image_pub.getNumSubscribers() == 0
@@ -161,32 +184,14 @@ int main(int argc, char** argv) {
 
         pcl::fromROSMsg(*m, cloud);
 
-        sensor_msgs::Image range_image;
-        sensor_msgs::Image ambient_image;
-        sensor_msgs::Image intensity_image;
+        // sensor_msgs::Image range_image;
+        // sensor_msgs::Image ambient_image;
+        // sensor_msgs::Image intensity_image;
 
-        range_image.width = W;
-        range_image.height = H;
-        range_image.step = W;
-        range_image.encoding = encoding;
-        range_image.data.resize(W * H * bit_depth /
-                                (8 * sizeof(*range_image.data.data())));
         range_image.header.stamp = m->header.stamp;
 
-        ambient_image.width = W;
-        ambient_image.height = H;
-        ambient_image.step = W;
-        ambient_image.encoding = encoding;
-        ambient_image.data.resize(W * H * bit_depth /
-                                  (8 * sizeof(*ambient_image.data.data())));
         ambient_image.header.stamp = m->header.stamp;
 
-        intensity_image.width = W;
-        intensity_image.height = H;
-        intensity_image.step = W;
-        intensity_image.encoding = encoding;
-        intensity_image.data.resize(W * H * bit_depth /
-                                    (8 * sizeof(*intensity_image.data.data())));
         intensity_image.header.stamp = m->header.stamp;
 
         ouster::img_t<double> ambient_image_eigen(H, W);
@@ -203,17 +208,13 @@ int main(int argc, char** argv) {
                         range_image.data.data())[u * W + v] = 0;
                 } else {
                     double pixel_value = std::min(std::round(pt.range * range_multiplier * 1.0/1000.0 * pixel_value_max), static_cast<double>(pixel_value_max));
-                    reinterpret_cast<pixel_type*>(
+                    reinterpret_cast<pixel_type*>( //use a unit test to get a pt.range
                         range_image.data.data())[u * W + v] =
                         pixel_value_max - pixel_value;
                         // pixel_value_max -
                         // std::min(std::round(pt.range * range_multiplier),
                         //          static_cast<double>(pixel_value_max));
-                    ROS_INFO("Old range_image.data.data())[u * W + v] %f", pt.range * range_multiplier);
-                    ROS_WARN("NEW range_image.data.data())[u * W + v] %f", pixel_value);
                 }
-
-                ROS_INFO("pt.range %d", pt.range);
                 ambient_image_eigen(u, v) = pt.ambient;
                 intensity_image_eigen(u, v) = pt.intensity;
             }
